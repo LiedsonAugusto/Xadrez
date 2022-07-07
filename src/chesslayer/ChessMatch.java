fb6142c300197926259389c54c8f6b19a649ee6a
Liedson <liedsonaugusto@gmail.com>
2022-07-07 11:19:11 -0300

    Adicionando sistema de cores nas peças

--- src/chesslayer/ChessMatch.java
+++ src/chesslayer/ChessMatch.java
@@ -12,8 +12,8 @@ public class ChessMatch {
 		board = new Board(8, 8);
 		initialSetup();
 	}
-	
-	public ChessPiece[][] getPieces(){
+
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] temp = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
@@ -22,13 +22,24 @@ public class ChessMatch {
 		}
 		return temp;
 	}
-	
+
 	private void PlaceNewPiece(char column, int row, ChessPiece piece) {
 		board.PlacePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		PlaceNewPiece('d', 1, new Rook(board, Color.BLACK));
-		PlaceNewPiece('a', 1, new King(board, Color.BLACK));
+		PlaceNewPiece('c', 1, new Rook(board, Color.WHITE));
+		PlaceNewPiece('c', 2, new Rook(board, Color.WHITE));
+		PlaceNewPiece('d', 2, new Rook(board, Color.WHITE));
+		PlaceNewPiece('e', 2, new Rook(board, Color.WHITE));
+		PlaceNewPiece('e', 1, new Rook(board, Color.WHITE));
+		PlaceNewPiece('d', 1, new King(board, Color.WHITE));
+
+		PlaceNewPiece('c', 7, new Rook(board, Color.BLACK));
+		PlaceNewPiece('c', 8, new Rook(board, Color.BLACK));
+		PlaceNewPiece('d', 7, new Rook(board, Color.BLACK));
+		PlaceNewPiece('e', 7, new Rook(board, Color.BLACK));
+		PlaceNewPiece('e', 8, new Rook(board, Color.BLACK));
+		PlaceNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
